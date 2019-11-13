b8d278ee19b0f5577d0fc40a6d0a8abe351105be
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2019-11-12 22:25:57 -0800

    Adding Jenking pipeline.

--- devops/build_pipeline.groovy
+++ devops/build_pipeline.groovy
@@ -2,6 +2,17 @@
 node {
   def TestService = 'TestService'
 
+  stage('Checkout from Git') {
+  		checkout ([
+  		$class: 'GitSCM',
+  		branches: [[name: '*/master']],
+  		extensions: [
+  				[$class: 'CleanCheckout']
+  		],
+  		userRemoteConfigs: [[credentialsId: 'admin', url: 'https://github.com/systeam-org/seller-service']]
+  		])
+  }
+
   stage('Run Test Cases') {
     build_ddr = build job: "${TestService}",
       parameters: [
