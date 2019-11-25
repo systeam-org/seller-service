0560faa255499dc2a6f80f48f56603a81c9db4cb
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2019-11-25 13:39:14 -0800

    Updated build pipeline.

--- devops/build_pipeline.groovy
+++ devops/build_pipeline.groovy
@@ -1,7 +1,12 @@
 #!/usr/bin/env groovy
 node {
-  def TestServiceJob = 'TestService'
+  def TestServiceJob = 'TestSellerService'
   def BuildContainerJob = 'BuildContainer'
+  def GitOrganization = "systeam-org"
+  def ServiceGitRepo = "seller-service"
+  def DockerHubOrganization = "systeamorg"
+  def ServiceDockerHubRepo = "seller-service"
+
 
   stage('Checkout from Git') {
   		checkout ([
@@ -10,23 +15,21 @@ node {
   		extensions: [
   				[$class: 'CleanCheckout']
   		],
-  		userRemoteConfigs: [[credentialsId: 'admin', url: 'https://github.com/systeam-org/seller-service']]
+  		userRemoteConfigs: [[credentialsId: 'admin', url: 'https://github.com/${GitOrganization}/${ServiceGitRepo}']]
   		])
   }
 
   stage('Execute API Tests') {
-    build_service = build job: "${TestServiceJob}",
-      parameters: [
-        string(name: 'ServiceName', value: "Seller")
-
-      ]
+    build_service = build job: "${TestServiceJob}"
   }
 
   stage('Build Docker Image') {
     build_ddr = build job: "${BuildContainerJob}",
     parameters: [
-        string(name: 'GitRepo', value: "SellerGit")
-
+        string(name: 'GitOrganization', value: "${GitOrganization}"),
+        string(name: 'ServiceGitRepo', value: "${ServiceGitRepo}"),
+        string(name: 'DockerHubOrganization', value: "${DockerHubOrganization}"),
+        string(name: 'ServiceDockerHubRepo', value: "${ServiceDockerHubRepo}")
       ]
   }
 
